04f0aa9ad6cbb9859ff55ba3a4018f42accf5801
joelward2025 <104170342+joelward2025@users.noreply.github.com>
2024-09-20 14:05:51 -0400

    Initial Commit

---
+++ Lab05/Lab05App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab05App.swift
+//  Lab05
+//
+//  Created by Joel Ward on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
